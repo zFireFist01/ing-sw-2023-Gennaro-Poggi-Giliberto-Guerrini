e14f82405d6ffbd07bd155808690582382b8a57c
IoSonoDue2 <valentino.guerrini01@gmail.com>
2023-03-25 17:21:52 +0100

    added match class skeleton with attributes and methods to be implemented

--- src/main/java/Server/Model/Match.java
+++ src/main/java/Server/Model/Match.java
@@ -1,7 +1,93 @@
 package Client;
 
+import Server.Model.LivingRoom;
+
+import java.sql.Time;
+
+/**
+ * match class in order to store information about the match
+ *
+ */
 public class Match {
+    private Player[] players;
     private final int numberOfPlayers;
-    
+    private CommnoGoalCard[] commonGoals;
+    private MatchStatus matchStatus;
+    private final Player matchOpener;
+    private Player firstPlayer;
+    private Player currentPlayer;
+    private Player winner;
+    private final LivingRoom livingRoom;
+    private final CommonGoalCardsDeck commonGoalDeck;
+    private final PersonalGoalCardsDeck personalGoalDeck;
+    private map<Player, int> scores;
+    private Time matchDuration;
+
+    public Match(int numberOfPlayers, Player matchOpener) {
+        this.numberOfPlayers = numberOfPlayers;
+        this.matchOpener = matchOpener;
+        this.livingRoom = new LivingRoom();
+        this.commonGoalDeck = new CommonGoalCardsDeck();
+        this.personalGoalDeck = new PersonalGoalCardsDeck();
+    }
+
+    public void setup(){}
+
+    public void checkCommonGoals(Player){}
+
+    private void extractFirstPlayer(){}
+
+    private void extractCommonGoals(){}
+
+    public void addContestant(){}
+
+    public void calculateFinalScores(){}
+
+
+    public void setCurrentPlayer(Player currentPlayer) {
+        this.currentPlayer = currentPlayer;
+    }
+
+    public void setWinner(Player winner) {
+        this.winner = winner;
+    } //secondo me andrebbe messo getwinner il winner viene settato in calculatefinalscores
+
+    //METODI GETTER
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public CommnoGoalCard[] getCommonGoals() {
+        return commonGoals;
+    }
+
+    public MatchStatus getMatchStatus() {
+        return matchStatus;
+    }
+
+    public Player getFirstPlayer() {
+        return firstPlayer;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public LivingRoom getLivingRoom() {
+        return livingRoom;
+    }
+
+    public map<Player, int> getScores() {
+        return scores;
+    }
+
+    public Time getMatchDuration() {
+        return matchDuration;
+    }
+
+    public int getNumberOfPlayers() {
+        return numberOfPlayers;
+    }
 
 }
