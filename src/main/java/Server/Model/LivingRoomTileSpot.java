6292a27618e39db36581f5dad140a0eee43b7964
Patrick <patrick.poggi@mail.polimi.it>
2023-03-26 21:22:28 +0200

    added some classes and some early methods implementations

--- src/main/java/Server/Model/LivingRoomTileSpot.java
+++ src/main/java/Server/Model/LivingRoomTileSpot.java
@@ -1,2 +1,13 @@
-package Server.Model;public class LivingRoomTileSpot {
+package Server.Model;
+public class LivingRoomTileSpot extends TileSpot{
+    private int dotsNumber;
+    private Match m;
+    protected boolean isReal(){
+        return dotsNumber >= 0 && dotsNumber <= m.getNumberOfPlayers();
+    }
+
+    protected int getDotsNumber(){
+        return dotsNumber;
+    }
+
 }
--- src/main/java/Server/Model/TileSpot.java
+++ src/main/java/Server/Model/TileSpot.java
@@ -1,2 +1,14 @@
-package Server.Model;public class TileSpot {
+package Server.Model;
+public class TileSpot {
+    private TileType tile;
+
+    protected boolean isEmpty(){
+        return tile==null;
+    }
+
+    protected void setTile(TileType type){}
+
+    protected TileType getTileType(){
+        return tile;
+    }
 }
