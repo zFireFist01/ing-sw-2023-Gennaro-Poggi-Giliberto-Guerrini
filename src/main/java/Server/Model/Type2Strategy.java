300ba6255b8b258fcdb0c55b18cb8d4412349dc3
marta23gili <marta.giliberto@mail.polimi.it>
2023-03-27 22:24:52 +0200

    write Type2Strategy

--- src/main/java/Server/Model/Type2Strategy.java
+++ src/main/java/Server/Model/Type2Strategy.java
@@ -2,19 +2,20 @@ package Server.Model;
 
 public class Type2Strategy implements CommonGoalCheckingStrategy {
     private boolean check;
-    private TileType tmp;
     private BookshelfTileSpot[][] b;
-    private int i;
-    private int j;
 
     @Override
     public boolean apply(Bookshelf bookshelf) {
 
-        b= bookshelf.getTileMatrix();
+        b = bookshelf.getTileMatrix();
 
-
-
-
+        if ((b[0][0].isEmpty()) || (b[0][4].isEmpty()) || (b[5][0].isEmpty()) || (b[5][4].isEmpty())) {
+            return check;
+        } else if ((b[5][0].getTile().equals(b[5][4].getTile())) &&
+                (b[0][0].getTile().equals(b[0][4].getTile())) &&
+                (b[0][0].getTile().equals(b[5][0].getTile()))) {
+            check = true;
+        }
 
 
         return check;
