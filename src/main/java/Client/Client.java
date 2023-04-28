2b96a37fd19d81faa3a893da729c2fc2b957360f
IoSonoDue2 <valentino.guerrini01@gmail.com>
2023-04-29 00:57:07 +0200

    started to write  client: main, network handler, CLI

---
+++ src/main/java/Client/Client.java
@@ -0,0 +1,93 @@
+package Client;
+
+import Client.View.CLI.CLI;
+
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        if (args.length > 1) {
+            switch (args[0]) {
+                case "-gui" :
+                    //TODO GUI
+                case "-cli" :
+                System.out.println("Welcome to");
+                System.out.println("                                                                                                    \n" +
+                        "\n" +
+                        "                                          ▄▒░▄░▒                                                    \n" +
+                        "\n" +
+                        "        ▐░░░▓    ░░░░                    ▐░▐▀░░░░   ▄                 ░░▄   ░▄▄░                    \n" +
+                        "\n" +
+                        "         ▐░░▌    ▒░░▌                    ▐░▐  ▒▄▓   ▐░▐▀              ░▒ ▐░▄▀░▒ ░▓                  \n" +
+                        "\n" +
+                        "         ▐░░▐    ░░░            ▄ ▄       ░░░       ▐░▐               ░▒ ▌░▌ ▒░▄▄▀                  \n" +
+                        "\n" +
+                        "         ▐░▐▒▒  ░▌░░     ░▒░▀  ▄░▐█        ▒░░▒     ▐░▐▒░░▄           ░▒ ▐░▌    ▄░                  \n" +
+                        "\n" +
+                        "         ▐▒▐ ░▒░▓ ░▐       ▒░░▒░▄▀            ▒░░░  ▐░░░▄░░░▌   ░▄▄   ▒▒  ▒▐     ▄▀   ▄▄▄░          \n" +
+                        "\n" +
+                        "         ▐▒▐ ▐▒▐ ▐▒▐        ▀░▒▄▀         ▄    ▐░░▒ ▐▒░▀  ▒▒▌ ▒░▀  ▐░ ▒▒ ▐▀▒░▀▀ ▒▒▓ ▒░█   ▒▌        \n" +
+                        "\n" +
+                        "         ▐▒▒ ▐▄▄ ▒▒▐  ▄▒▒▒▄ ▒░█         ▒░▒░▒▄  ▒▒▐ ▐▒▐   ▒░ ▐▒▒▀▀▀▒▀ ▒▒   ▐▒▌  ▒▒▌ ▌▒▀▀▀▀▀▀        \n" +
+                        "\n" +
+                        "         ▄▒▒▌    ▒▒▒ ▐▌▒▒▒▀▒░█          ▒▒▒▒▒▌▄▒▒▓▌ ▐▒▒   ▒▒  ▒▒  ▒▒▒▌▒▒   ▐▒█  ▒▒▌ ▒▒▒ ▐▒▒▓        \n" +
+                        "\n" +
+                        "         ▓▒▒▒   ▐▒▒▒▓  ▒▒▒▄▓▀             ▀▒▓▒▒▓█▀  ▒▒▒▌ ▐▒▒█  ▀▒▓▓▓▀ ▒▒▌  ▒▓▓  ▒▓▀  ▀▒▒▒▓▀         \n" +
+                        "\n" +
+                        "                                                                                                    ");
+                new CLI();
+                break;
+            }
+
+        } else {
+            System.out.println("Welcome to");
+            System.out.println("                                                                                                    \n" +
+                    "\n" +
+                    "                                          ▄▒░▄░▒                                                    \n" +
+                    "\n" +
+                    "        ▐░░░▓    ░░░░                    ▐░▐▀░░░░   ▄                 ░░▄   ░▄▄░                    \n" +
+                    "\n" +
+                    "         ▐░░▌    ▒░░▌                    ▐░▐  ▒▄▓   ▐░▐▀              ░▒ ▐░▄▀░▒ ░▓                  \n" +
+                    "\n" +
+                    "         ▐░░▐    ░░░            ▄ ▄       ░░░       ▐░▐               ░▒ ▌░▌ ▒░▄▄▀                  \n" +
+                    "\n" +
+                    "         ▐░▐▒▒  ░▌░░     ░▒░▀  ▄░▐█        ▒░░▒     ▐░▐▒░░▄           ░▒ ▐░▌    ▄░                  \n" +
+                    "\n" +
+                    "         ▐▒▐ ░▒░▓ ░▐       ▒░░▒░▄▀            ▒░░░  ▐░░░▄░░░▌   ░▄▄   ▒▒  ▒▐     ▄▀   ▄▄▄░          \n" +
+                    "\n" +
+                    "         ▐▒▐ ▐▒▐ ▐▒▐        ▀░▒▄▀         ▄    ▐░░▒ ▐▒░▀  ▒▒▌ ▒░▀  ▐░ ▒▒ ▐▀▒░▀▀ ▒▒▓ ▒░█   ▒▌        \n" +
+                    "\n" +
+                    "         ▐▒▒ ▐▄▄ ▒▒▐  ▄▒▒▒▄ ▒░█         ▒░▒░▒▄  ▒▒▐ ▐▒▐   ▒░ ▐▒▒▀▀▀▒▀ ▒▒   ▐▒▌  ▒▒▌ ▌▒▀▀▀▀▀▀        \n" +
+                    "\n" +
+                    "         ▄▒▒▌    ▒▒▒ ▐▌▒▒▒▀▒░█          ▒▒▒▒▒▌▄▒▒▓▌ ▐▒▒   ▒▒  ▒▒  ▒▒▒▌▒▒   ▐▒█  ▒▒▌ ▒▒▒ ▐▒▒▓        \n" +
+                    "\n" +
+                    "         ▓▒▒▒   ▐▒▒▒▓  ▒▒▒▄▓▀             ▀▒▓▒▒▓█▀  ▒▒▒▌ ▐▒▒█  ▀▒▓▓▓▀ ▒▒▌  ▒▓▓  ▒▓▀  ▀▒▒▒▓▀         \n" +
+                    "\n" +
+                    "                                                                                                    ");
+            new Client().Setup();
+        }
+    }
+
+    public void Setup(){
+        boolean flag=true;
+        int choice;
+
+        while(flag){
+            System.out.println("Enter 1 for CLI and 2 for GUI");
+            Scanner sc = new Scanner(System.in);
+            choice = sc.nextInt();
+            if(choice==1){
+                new CLI();
+                flag=false;
+            }
+            else if(choice==2){
+                //TODO gui
+                flag=false;
+            }
+            else {
+                System.out.println("Invalid choice");
+            }
+        }
+    }
+
+}
