ba0ce8d0b9baa8340b6c56170fadc475b04cacc1
marta23gili <marta.giliberto@mail.polimi.it>
2023-04-29 19:21:39 +0200

    Implenting NetworkSocketHandler

--- src/main/java/Client/NetworkSocketHandler.java
+++ src/main/java/Client/NetworkSocketHandler.java
@@ -1,10 +1,118 @@
 package Client;
 
-import Server.Network.Server;
+import Server.Events.MVEvents.MVEvent;
+import Server.Events.MVEvents.ModifiedLivingRoomEvent;
+import Server.Events.VCEvents.VCEvent;
+import Server.Network.VirtualView;
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class NetworkSocketHandler implements NetworkHandler{
-    public NetworkSocketHandler(Socket client, ServerSocket server) {
+    Socket socket;
+    ServerSocket server;
+    Scanner in;
+    OutputStream out;
+    public NetworkSocketHandler(ServerSocket server) {
+        socket = new Socket();
+        try {
+            in = new Scanner(socket.getInputStream());
+            out = socket.getOutputStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void sendMVEvent(String json) throws RemoteException {
+
+    }
+
+    @Override
+    public void sendSelectViewEvent(String json) throws RemoteException {
+
+    }
+
+    @Override
+    public void onVCEvent(VCEvent event, VirtualView view) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        throw new IllegalAccessError("This method should not be called");
+    }
+
+    @Override
+    public void onVCEvent(VCEvent event) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Gson gson = new Gson();
+        String json = gson.toJson(event);
+        try {
+            out.write(json.getBytes());
+            out.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void run() {
+        String message;
+        String primaryType;
+        String secondaryType;
+        Event event;
+        while (true){
+            message = in.nextLine();
+
+            Gson gson = new Gson();
+            Event event = gson.fromJson(message, Event.class);
+            primaryType = event.getPrimaryType();
+
+            switch (primaryType){
+                case "MVEvent":
+                    secondaryType = (MVEvent)event.getType();
+                    switch (secondaryType){
+                        case "ModifiedPointsEvent":
+                            ModifiedPointsEvent modifiedPointsEvent = gson.fromJson(message, ModifiedPointsEvent.class);
+                            break;
+                        case "ModifiedBookshelfEvent":
+                            ModifiedBookshelfEvent modifiedBookshelfEvent = gson.fromJson(message,ModifiedBookshelfEvent.class);
+                            break;
+                        case "ModifiedChatEvent":
+                            ModifiedChatEvent modifiedChatEvent = gson.fromJson(message, ModifiedChatEvent.class);
+                            break;
+                        case "ModifiedLivingRoomEvent":
+                            ModifiedLivingRoomEvent modifiedLivingRoomEvent = gson.fromJson(message, ModifiedLivingRoomEvent.class);
+                            break;
+                        case "ModifiedMatchEndedEvent":
+                            ModifiedMatchEndedEvent modifiedMatchEndedEvent = gson.fromJson(message, ModifiedMatchEndedEvent.class);
+                            break;
+                        default:
+                            throw new RuntimeException("Unknown event type");
+                    }
+                    break;
+                case "SelectViewEvent":
+                    secondaryType = (SelectViewEvent)event.getType();
+                    switch (secondaryType){
+
+                        case "EndedMatchEvent":
+                            EndedMatchEvent endedMatchEvent = gson.fromJson(message, EndedMatchEvent.class);
+                            break;
+                        case "ChatONEvent":
+                            ChatONEvent chatOFFEvent = gson.fromJson(message, ChatONEvent.class);
+                            break;
+                        case "ChatOFFEvent":
+                            ChatOFFEvent chatOFFEvent = gson.fromJson(message, ChatOFFEvent.class);
+                            break;
+                        default:
+                            throw new RuntimeException("Unknown event type");
+                    }
+                    break;
+                default:
+                    throw new RuntimeException("Unknown event type");
+            }
+        }
     }
 }
--- src/main/java/Server/Network/VirtualSocketView.java
+++ src/main/java/Server/Network/VirtualSocketView.java
@@ -5,9 +5,15 @@ import Server.Events.MVEvents.MVEvent;
 import Server.Events.SelectViewEvents.SelectViewEvent;
 import Server.Events.VCEvents.VCEvent;
 import Server.Listeners.VCEventListener;
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class VirtualSocketView implements VirtualView{
 
@@ -16,15 +22,43 @@ public class VirtualSocketView implements VirtualView{
 
     List<VCEventListener> vcEventListeners;
 
-    public VirtualSocketView(Socket client) {
+    public VirtualSocketView(Socket socket) {
         this.client = client;
+        this.vcEventListeners= new ArrayList<>();
     }
 
     @Override
     public void run(){
-
+        Scanner in;
+        OutputStream out;
+        try {
+            in = new Scanner(socket.getInputStream());
+            out = socket.getOutputStream();
+        } catch (IOException e) {
+            System.err.println(e.getStackTrace());
+            throw new RuntimeException(e);
+        }
+        while (true){
+            String message = in.nextLine();
+            manageMessage(message);
+        }
     }
 
+    private void manageMessage(String message){
+        Gson gson = new Gson();
+        VCEvent vcEvent = gson.fromJson(message, VCEvent.class);
+        for(VCEventListener listener: vcEventListeners){
+            try {
+                listener.onVCEvent(vcEvent);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
     @Override
     public void onMVEvent(MVEvent mvEvent){
 
@@ -38,4 +72,16 @@ public class VirtualSocketView implements VirtualView{
     public void sendVCEvent(VCEvent vcEvent){
 
     }
+
+    @Override
+    public void addVCEventListener(VCEventListener listener){
+        vcEventListeners.add(listener);
+    }
+
+    @Override
+    public void removeVCEventListener(VCEventListener listener){
+        vcEventListeners.remove(listener);
+    }
+
+
 }
