039bf477429bfa63d5756526a2912801df3a838c
marta23gili <marta.giliberto@mail.polimi.it>
2023-06-04 20:29:29 +0200

    Add the possibility to see the bookshelf after the match is ended, add a background to the bookshelf of the player is playing, change the checkout button

--- src/main/java/Server/Model/GameItems/PointsTile.java
+++ src/main/java/Server/Model/GameItems/PointsTile.java
@@ -4,7 +4,6 @@ public enum PointsTile {
     TWO_1, TWO_2, FOUR_1, FOUR_2,
     SIX_1, SIX_2, EIGHT_1, EIGHT_2,
     MATCH_ENDED;
-
     public static PointsTile randomPointsTile() {
         return values()[(int) (Math.random() * values().length)];
     }
