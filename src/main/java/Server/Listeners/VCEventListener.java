15eb70371e27739cca807ee984c49b1a80fc690a
IoSonoDue2 <valentino.guerrini01@gmail.com>
2023-06-29 21:27:41 +0200

    fix issuses in matchended

--- src/main/java/Server/Listeners/VCEventListener.java
+++ src/main/java/Server/Listeners/VCEventListener.java
@@ -4,6 +4,7 @@ import Server.Network.VirtualView;
 
 import java.lang.reflect.InvocationTargetException;
 
+
 public interface VCEventListener {
     public void onVCEvent(VCEvent event, VirtualView view) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException;
     public void onVCEvent(VCEvent event) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException;
