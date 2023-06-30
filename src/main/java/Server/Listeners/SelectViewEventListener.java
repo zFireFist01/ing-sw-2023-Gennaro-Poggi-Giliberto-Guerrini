38de3867d6af1766e5f8f886acfa6e9f0243c376
IoSonoDue2 <valentino.guerrini01@gmail.com>
2023-06-30 04:39:35 +0200

    write javadoc for listeners

--- src/main/java/Server/Listeners/SelectViewEventListener.java
+++ src/main/java/Server/Listeners/SelectViewEventListener.java
@@ -5,6 +5,10 @@ import Server.Events.SelectViewEvents.SelectViewEvent;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+
+/**
+ * The {@code SelectViewEventListener} interface represents an event listener for SelectViewEvents.
+ */
 public interface SelectViewEventListener {
     public void onSelectViewEvent(SelectViewEvent event);
 }
